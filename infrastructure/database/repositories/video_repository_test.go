25d7758fdbd15192636dafee39902137367808b5
xStrato <metalofgear@hotmail.com>
2022-01-15 09:12:49 -0300

    changed video_repository_test to use Sqlite in :memory:

--- infrastructure/database/repositories/video_repository_test.go
+++ infrastructure/database/repositories/video_repository_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestVideoRepositoryMethods(t *testing.T) {
-	db, err := gorm.Open(sqlite.Open("app_test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
