520e891ec1688ed91a71586457a4dda632cef050
xStrato <metalofgear@hotmail.com>
2022-02-27 23:40:14 -0300

    add Dockerfile for CI/CD

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"log"
 
 	"github.com/joho/godotenv"
-	"github.com/xStrato/video-encoder-golang/infrastructure/cmd"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 func init() {
@@ -17,9 +14,11 @@ func init() {
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	cmd.StartServer(db)
+	// db, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// cmd.StartServer(db)
+
+	println("The program is running...")
 }
